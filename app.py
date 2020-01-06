4f950a6ee9027653d4a7acc2fc4989adb35e0e96
inv0w <jonjoninfante@gmail.com>
2020-01-06 12:45:47 -0800

    API Keys working for sunrise/sunset

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
+import requests
+import json
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = Flask(__name__)
+API_KEY_MAP = os.getenv("API_KEY_MAP")
+API_KEY_GEO = os.getenv("API_KEY_GEO")
+
 
 @app.route('/')
 def index():
@@ -10,7 +18,25 @@ def index():
 @app.route('/sun')
 def sun():
     """Return Sunrise/Sunset Page"""
-    return render_template('sun.html')
+    city = request.args.get("search")
+    if city == None:
+        city = "San Francisco, CA"
+    params = {
+        'key': API_KEY_MAP,
+        'location': city
+        }
+    r = requests.get('http://open.mapquestapi.com/geocoding/v1/address', params=params)
+    lat = r.json()["results"][0]["locations"][0]["latLng"]["lat"]
+    long = r.json()["results"][0]["locations"][0]["latLng"]["lng"]
+    headers = {
+        'apiKey': API_KEY_GEO,
+        'lat': lat,
+        'long': long
+        }
+    s = requests.get('https://api.ipgeolocation.io/astronomy', params=headers)
+    sunrise = s.json()["sunrise"]
+    sunset = s.json()["sunset"]
+    return render_template('sun.html', sunrise=sunrise, sunset=sunset, city=city)
 
 @app.route('/stars')
 def stars():
